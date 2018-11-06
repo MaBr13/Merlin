5583eac2d0274b5fe97cc24caaf907754b7f3e6f
Bradaric <mbradar@uva.nl>
2018-11-06 15:05:38 +0100

    added google key and new radar locations

--- OWEZ/scripts/radarmaps.R
+++ OWEZ/scripts/radarmaps.R
@@ -3,11 +3,12 @@ library(ggplot2)
 library(gridExtra)
 library(ggpubr)
 
+register_google(key = "AIzaSyC-bw1FEcHmDesL-zAPgW9RaWwolld2gRw")
 
 #creating map
 
-NSea <- get_map(location = c(lon=3.8, lat=52.5110 ), 
-                maptype = "terrain-background",  source = "stamen", color="bw", zoom=7) #add a map
+NSea <- get_map(location = c(lon=3.8, lat=54.5110 ), 
+                maptype = "terrain-background",  source = "stamen", color="bw", zoom=6) #add a map
 S <- ggmap(NSea)
 S
 
@@ -15,15 +16,20 @@ S
 OWEZ <- c(52.60636,4.389639)
 K14 <- c(53.26889,3.628889)
 Luchterduinen <- c(52.41667,4.166667)
-Radars <- as.data.frame(rbind(OWEZ,K14))
-Radars <- as.data.frame(rbind(Radars,Luchterduinen))
-names <- c("OWEZ","K14","Luchterduinen")
-Radars <- as.data.frame(cbind(Radars,names))
-names(Radars)[c(1,2)]<-paste(c("Latitude","Longitude","Radar"))
+Thornton <- c(51.5328,2.95509)
+Den_Helder <- c(52.9563,4.7608)
+
+Radars <- as.data.frame(rbind(OWEZ,K14,Luchterduinen, Thornton, Den_Helder))
+names <- c("OWEZ","K14","Luchterduinen","Thornton", "Den Helder")
+type <- c("Bird radar","Bird radar", "Bird radar","Bird radar", "Weather radar")
+Radars <- as.data.frame(cbind(Radars,names,type))
+names(Radars)[c(1,2,3,4)]<-paste(c("Latitude","Longitude","Radar","Type"))
 
 
-d <- S+geom_point(data=Radars, aes_string(Radars$Longitude,Radars$Latitude,size=3, colour=Radars$Radar), show.legend = T) + 
-  #scale_colour_manual(values=c("darkmagenta", "dodgerblue4", "aquamarine3"), name="Radar")+
+d <- S+geom_point(data=Radars, aes_string(Radars$Longitude,Radars$Latitude, size=Radars$Radar,colour=Radars$Radar,shape=Radars$Type),show.legend = T) + 
+  scale_colour_manual(values=c("darkmagenta", "dodgerblue4", "aquamarine3","orange", "yellow"), name="Radar")+
+  scale_size_manual(values=c(5,5,5,5,5),name="Radar")+
+  scale_shape_manual(values=c(19,17), name="Radar type")+
   ylab("Latitude")+
   xlab  ("Longitude")
-  
+d  
