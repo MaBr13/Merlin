f9dbf625ea4e69a80654e614277b65a3588c5ae9
Bradaric <mbradar@uva.nl>
2018-11-21 11:07:56 +0000

    added extra day

--- OWEZ/scripts/hourly_feather_plots.R
+++ OWEZ/scripts/hourly_feather_plots.R
@@ -23,20 +23,24 @@ Sys.setenv(TZ="UTC")
 Sys.setlocale(category = "LC_ALL", locale = "English_United Kingdom.1252")#set the time on your computer to match
 
 #SELECT DAYS WITH HIGH MIGRATION
-Oct1a <- subset(Allyears[[1]], timestep>"2007-10-13 16:00:00" & timestep<"2007-10-14 16:00:00" , select = id:Wspeed)
-Oct2a <- subset(Allyears[[1]], timestep>"2007-10-20 16:00:00" & timestep<"2007-10-21 16:00:00", select = id:Wspeed)
-Oct3a <- subset(Allyears[[2]], timestep>"2008-10-30 16:00:00" & timestep<"2008-10-31 16:00:00", select = id:Wspeed)
-Mar1a <- subset(Allyears[[2]], timestep>"2008-03-28 16:00:00" & timestep<"2008-03-29 16:00:00", select = id:Wspeed)
-Mar2a <- subset(Allyears[[4]], timestep>"2010-03-16 16:00:00" & timestep<"2010-03-17 16:00:00", select = id:Wspeed)
+Oct1a <- subset(Allyears[[1]], timestep>="2007-10-13 16:00:00 UTC" & timestep<="2007-10-14 16:00:00 UTC" , select = id:Wspeed)
+Oct2a <- subset(Allyears[[1]], timestep>="2007-10-20 16:00:00 UTC" & timestep<="2007-10-21 16:00:00 UTC", select = id:Wspeed)
+Oct3a <- subset(Allyears[[2]], timestep>="2008-10-30 16:00:00 UTC" & timestep<="2008-10-31 16:00:00 UTC", select = id:Wspeed)
+Mar1a <- subset(Allyears[[2]], timestep>="2008-03-27 16:00:00 UTC" & timestep<="2008-03-28 16:00:00 UTC", select = id:Wspeed)
+Mar2a <- subset(Allyears[[2]], timestep>="2008-03-28 16:00:00 UTC" & timestep<="2008-03-29 16:00:00 UTC", select = id:Wspeed)
+Mar3a <- subset(Allyears[[4]], timestep>="2010-03-16 16:00:00 UTC" & timestep<="2010-03-17 16:00:00 UTC", select = id:Wspeed)
 
 Oct1m <- subset(means[[1]], Timestamp>"2007-10-13 16:00:00" & Timestamp<"2007-10-14 16:00:00", select = Timestamp:Rlight)
 Oct2m <- subset(means[[1]], Timestamp>"2007-10-20 16:00:00" & Timestamp<"2007-10-21 16:00:00", select = Timestamp:Rlight)
 Oct3m <- subset(means[[2]], Timestamp>"2008-10-30 16:00:00" & Timestamp<"2008-10-31 16:00:00", select = Timestamp:Rlight)
-Mar1m <- subset(means[[2]], Timestamp>"2008-03-28 16:00:00" & Timestamp<"2008-03-29 16:00:00", select = Timestamp:Rlight)
-Mar2m <- subset(means[[4]], Timestamp>"2010-03-16 16:00:00" & Timestamp<"2010-03-17 16:00:00", select = Timestamp:Rlight)
+Mar1m <- subset(means[[2]], Timestamp>"2008-03-27 16:00:00" & Timestamp<"2008-03-28 16:00:00", select = Timestamp:Rlight)
+Mar2m <- subset(means[[2]], Timestamp>"2008-03-28 16:00:00" & Timestamp<"2008-03-29 16:00:00", select = Timestamp:Rlight)
+Mar3m <- subset(means[[4]], Timestamp>"2010-03-16 16:00:00" & Timestamp<"2010-03-17 16:00:00", select = Timestamp:Rlight)
 
-DaysA <- list(Oct1a,Oct2a,Oct3a,Mar1a,Mar2a)
-DaysM <- list(Oct1m,Oct2m,Oct3m,Mar1m,Mar2m)
+
+DaysA <- list(Oct1a,Oct2a,Oct3a,Mar1a,Mar2a,Mar3a)
+DaysM <- list(Oct1m,Oct2m,Oct3m,Mar1m,Mar2m,Mar3m)
+
 
 library(dplyr)
 for (k in 1:length(DaysA)){
